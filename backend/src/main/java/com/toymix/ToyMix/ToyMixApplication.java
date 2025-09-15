b4f33d2f0efbe1cdbd7f891ef73f2cf667ad19e1
Maikon Henrique <maikonhenrique189@gmail.com>
2025-09-14 22:00:15 -0300

    feat: adiciona estrutura inicial do back end em Java (Spring Boot)

---
+++ backend/src/main/java/com/toymix/ToyMix/ToyMixApplication.java
@@ -0,0 +1,13 @@
+package com.toymix.ToyMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyMixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/toymix/ToyMix/ToyMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toymix.ToyMix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
