e969e17e9284b82222463e80dfe1e0030b586125
MateusdSB <mateus.dossantosbarbosa@gmail.com>
2025-09-21 13:54:37 -0300

    refactor: reorganização das categorias

--- fbc-brinquedos/src/app/data/categoria.ts
+++ fbc-brinquedos/src/app/data/categoria.ts
@@ -1,53 +1,36 @@
 import { Categoria } from '../interfaces/categoria';
 
 export const CATEGORIAS: Categoria[] = [
-  {
-    titulo: 'Veículos',
-    imagem: 'https://i.imgur.com/65zGku1.png',
-  },
-  {
+ {
     titulo: 'Bonecas',
     imagem: 'https://i.imgur.com/WL721d2.png',
-  },
-  {
-    titulo: 'Construção',
-    imagem: 'https://i.imgur.com/upev2RO.png',
   },
   {
     titulo: 'Pelúcia',
     imagem: 'https://i.imgur.com/yiQtoBj.png',
   },
   {
-    titulo: 'Quebra-Cabeça',
-    imagem: 'https://i.imgur.com/DU6Uyfn.png',
+    titulo: 'Veículos',
+    imagem: 'https://i.imgur.com/65zGku1.png',
   },
   {
-    titulo: 'Jogos',
-    imagem: 'https://i.imgur.com/qIqUkDZ.png',
+    titulo: 'Blocos de montar',
+    imagem: 'https://i.imgur.com/upev2RO.png',
   },
   {
-    titulo: 'Profissões',
-    imagem: 'https://i.imgur.com/yP5q8tp.png',
+    titulo: 'Jogos e Tabuleiro',
+    imagem: 'https://media.gettyimages.com/id/182187132/pt/foto/boardgame-pe%C3%A7as.jpg?s=1024x1024&w=gi&k=20&c=tIkUH700fePk8jsqe_bTuE5w7QzQEC6ioMRZ35636eQ=',
   },
   {
-    titulo: 'Esportes',
-    imagem: 'https://i.imgur.com/qIqUkDZ.png',
-  },
-  {
-    titulo: 'Ação',
+    titulo: 'Bonecos',
     imagem: 'https://i.imgur.com/fSIYd2h.png',
   },
   {
-    titulo: 'Artes',
+    titulo: 'Profissões e Faz de Conta',
+    imagem: 'https://i.imgur.com/yP5q8tp.png',
+  },
+  {
+    titulo: 'Diversos',
     imagem: 'https://i.imgur.com/GyZDjzM.png',
   },
-
-  // {
-  //   titulo: 'Tecnologia',
-  //   imagem: 'assets/img/category/tecnologia.png',
-  // },
-  // {
-  //   titulo: 'Musical',
-  //   imagem: 'https://i.imgur.com/9vwGmPb.png',
-  // },
 ];
