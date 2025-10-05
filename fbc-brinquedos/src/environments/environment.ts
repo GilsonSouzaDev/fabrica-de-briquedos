75c5e55c2f6e6c3283271f46795399abcce033e0
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-10-05 10:50:08 -0300

    feat: configurando deploy

--- fbc-brinquedos/src/environments/environment.ts
+++ fbc-brinquedos/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMockAuth: false, // 👈 troca aqui entre mock e api
-  //apiUrl: 'https://toymix-production.up.railway.app',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://toymix-production.up.railway.app',
+  //apiUrl: 'http://localhost:8080',
 };
