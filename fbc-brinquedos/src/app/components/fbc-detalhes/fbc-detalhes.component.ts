c89c6c5c8f3ba23daff05b3960767742b29e0434
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-10-05 23:38:21 -0300

    refactor: atualizando moeda corrente em detalhes

--- fbc-brinquedos/src/app/components/fbc-detalhes/fbc-detalhes.component.ts
+++ fbc-brinquedos/src/app/components/fbc-detalhes/fbc-detalhes.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Brinquedo } from '../../interfaces/brinquedo';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { RealPipe } from "../../pipes/real.pipe";
 
 
 @Component({
   selector: 'app-fbc-detalhes',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RealPipe],
   templateUrl: './fbc-detalhes.component.html',
   styleUrl: './fbc-detalhes.component.scss'
 })
