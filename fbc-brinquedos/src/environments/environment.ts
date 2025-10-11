f40ef96874d166db5e4d9218caf335d6171938e3
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-10-11 17:55:54 -0300

    refactor: atualizando commit externo

--- fbc-brinquedos/src/environments/environment.ts
+++ fbc-brinquedos/src/environments/environment.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
   useMockAuth: false, // 👈 troca aqui entre mock e api
-<<<<<<< HEAD
   apiUrl: 'https://toymix-production-d1e5.up.railway.app',
   // apiUrl: 'http://localhost:8080',
   // apiUrl: 'https://toymix.onrender.com',
-=======
-  apiUrl: 'https://toymix-production-d1e5.up.railway.app',
-  // apiUrl: 'http://localhost:8080',
-  // apiUrl: 'https://toymix.onrender.com',
->>>>>>> refs/remotes/origin/main
 };
