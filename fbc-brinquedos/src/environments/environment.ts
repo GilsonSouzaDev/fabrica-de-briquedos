1ae80e69f59e107dcacdf8b5e18b3921cd0a8b77
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2026-08-16 03:24:47 -0300

    chore: migrate backend and frontend to oracle vm

--- fbc-brinquedos/src/environments/environment.ts
+++ fbc-brinquedos/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   useMockAuth: false, // 👈 troca aqui entre mock e api
   // apiUrl: 'https://toymix-production-d1e5.up.railway.app',
   // apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://toymix-api.onrender.com',
+  apiUrl: 'https://fabricabrinquedos.duckdns.org',
 };
